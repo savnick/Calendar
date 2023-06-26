b96df417efc2d230ed147cc5fd0043f484939178
Nick Savenkov <savnick@icloud.com>
2023-06-26 09:38:27 +0300

    Initial Commit

---
+++ Calendar/CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarApp.swift
+//  Calendar
+//
+//  Created by Nick on 26.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
